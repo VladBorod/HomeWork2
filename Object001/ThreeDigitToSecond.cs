2bc677b8123dc369e8d91d3466344c11afa3df07
VladBorod <jazgigantsmith@gmail.com>
2022-11-16 14:40:49 +0300

    Final edition

---
+++ Object001/ThreeDigitToSecond.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.WriteLine($"Please enter three-digit number");
+
+int numberTD;
+
+while (true)
+{
+    Console.Write("Enter three-digit number: ");
+    if(int.TryParse(Console.ReadLine(), out numberTD))
+        break;
+    Console.WriteLine("Input error!");
+}
+// Console.WriteLine($"Are you shure it's a three-digit number? -> {numberTD}");
+
+int numberSecondThird = numberTD % 100;
+int numberSecond = numberSecondThird / 10;
+
+Console.WriteLine($"{numberTD} -> {numberSecond}");
+
