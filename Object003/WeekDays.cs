90d08243e6043955f3fe8b2089cadc7a945d9f7c
VladBorod <jazgigantsmith@gmail.com>
2022-11-18 15:23:28 +0300

    Object 3 final edition

---
+++ Object003/WeekDays.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.Write("Please enter day number: ");
+
+int number;
+
+while (true)
+{
+    if(int.TryParse(Console.ReadLine(), out number))
+        break;  
+    Console.WriteLine("Input error!!!");
+}
+
+Convert.ToInt32(number);
+
+if (number < 6)
+{
+    Console.WriteLine($"{number} -> нет");
+}
+if (number == 6)
+{
+    Console.WriteLine($"{number} -> да");
+}
+if (number == 7)
+{
+    Console.WriteLine($"{number} -> да");
+}
+if (number > 7)
+{
+    Console.WriteLine($"{number} -> несуществующий день недели");
+}
